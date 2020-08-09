38ddd680e6df8132319dfb4e67f4751c26b675a3
Diego <diegosl@hotmail.cl>
2020-08-08 22:19:36 -0400

    Juego terminado y se agrega Readme.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Juego Snake
+
+Juego desarrollado con Python y libreria Pygame.
+Consiste en manejar una serpiente y comer frutas para crecer, cuidando de no chocar consigo misma o los extremos de la pantalla
+
+## `Para correr el juego`
+
+Para poder correr el juego es necesario seguir los siguientes pasos:
+
+### `Instalar Pygame`
+
+Para instalar Pygame usa el siguiente comando:
+(Usamos --user para instalar en el directorio home y no globalmente)
+
+    python3 -m pip install -U pygame --user
+
+### `Clonar repositorio`
+
+Luego debes clonar el repositorio con el juego:
+
+    git clone https://github.com/diego-sepulveda-lavin/snake-game.git
+
+### `Correr el juego`
+
+Ejecutar python o python3:
+    python3 game.py
--- game.py
+++ game.py
@@ -14,6 +14,7 @@ food_color = (204, 0, 0)
 background_color = (0, 0, 0)
 game_over_color = (255, 0, 0)
 score_color = (0, 255, 0)
+aviso_color = (255, 255, 0)
 
 # Seteo de variables para tamaño de ventana
 display_width = 600
@@ -30,7 +31,7 @@ snake_speed = 15
 
 # Definir tamaño bloque snake
 snake_block = 10
-snake_list = []
+
 
 # Define forma y posicion de la snake
 def snake(snake_block, snake_list):
@@ -40,6 +41,7 @@ def snake(snake_block, snake_list):
 
 # Funcion principal del juego
 def snake_game():
+    global snake_speed
     game_end = False  # Flag para terminar juego
     game_over = False  # Flag juego perdido
 
@@ -73,14 +75,14 @@ def snake_game():
             game_window.blit(msg_perdiste, [display_width / 6, display_height / 6])  # Dibuja mensaje sobre surface
 
             # Mostrar el puntaje al final de juego
-            score = snake_length - 1
+            score = (snake_length - 1) * 10
             score_font_style = pygame.font.SysFont("comicsansms", 35)
-            value = score_font_style.render(f"Tu puntaje es: {str(score)}", True, score_color)
+            value = score_font_style.render(f"Tu puntaje fue: {str(score)}", True, score_color)
             game_window.blit(value, [display_width / 6, display_height / 4]) # Dibuja mensaje sobre surface
 
             # Mostrar la opcion nuevo juego
             nuevo_juego_font_style = pygame.font.SysFont("comicsansms", 20)
-            msg_nuevo_juego = nuevo_juego_font_style.render("Para jugar nuevamente presiona Enter o ESC para salir..", True, game_over_color)
+            msg_nuevo_juego = nuevo_juego_font_style.render("Para jugar nuevamente presiona Enter o ESC para salir..", True, aviso_color)
             game_window.blit(msg_nuevo_juego, [display_width / 6, display_height / 2]) # Dibuja mensaje sobre surface
 
             # Hace update de pantalla
@@ -101,6 +103,7 @@ def snake_game():
                     game_end = True
                     game_over = False
 
+        # Bloque de juego
         for event in pygame.event.get():
             
             # Termina el juego
@@ -125,31 +128,62 @@ def snake_game():
         # Condicion de game over al tocar bordes de la pantalla
         if x1 >= display_width or x1 < 0 or y1 >= display_height or y1 < 0:
             game_over = True
-            
-        # updated coordinates with the changed positions
+
+        # Actualiza coordenadas segun input de teclas capturadas
         x1 += x1_change
         y1 += y1_change
+
+        # Genera superficie y dibuja comida
         game_window.fill(background_color)
         pygame.draw.rect(game_window, food_color, [food_pos_x, food_pos_y, snake_block, snake_block])
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
+
+        # Contador de score
+        score = (snake_length - 1) * 10
+        score_font_style = pygame.font.SysFont("comicsansms", 20)
+        value = score_font_style.render(f"Puntos: {str(score)}", True, score_color)
+        game_window.blit(value, [270, 1])
+
+        # Mensaje de animo
+        if score % 50 == 0 and score != 0:
+            animo_font_style = pygame.font.SysFont("comicsansms", 20)
+            animo = score_font_style.render(f"Wow {str(score)} puntos, sigue asi!", True, aviso_color)
+            game_window.blit(animo, [220, 15])
+        
+        # Mensaje de velocidad
+        velocidad_font_style = pygame.font.SysFont("comicsansms", 20)
+        velocidad = score_font_style.render(f"Velocidad {str(snake_speed)}", True, aviso_color)
+        game_window.blit(velocidad, [260, 380])
+
+        # Se crea cabeza de snake
+        snake_head = [x1, y1]
+
+        # Se agrega la cabeza de snake al cuerpo
         snake_list.append(snake_head)
-        # when the length of the snake exceeds, delete the snake_list which will end the game
+
+        # Mantiene largo de la snake y genera movimiento
         if len(snake_list) > snake_length:
             del snake_list[0]
-        # when the snake hits itself, game ends
+
+        # Condicion cuando la snake choca consigo misma, entonces game over
         for x in snake_list[:-1]:
             if x == snake_head:
                 game_over = True
+        
+        # Llamar a la funcion snake
         snake(snake_block, snake_list)
+
+        # Hace update de pantalla
         pygame.display.update()
-        # when snake hits the food, the length of the snake is incremented by 1
+
+        # Condicion para incrementar el largo del cuerpo cuando encuentra comida y generar nuevo alimento
         if x1 == food_pos_x and y1 == food_pos_y:
             food_pos_x = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
             food_pos_y = round(random.randrange(0, display_height - snake_block) / 10.0) * 10.0
-            snake_length += 1
-        clock.tick(snake_speed)  # Metodo para actualizar el objeto clock, recibe framerate para controlar velocidad
+            snake_length += 1 
+            snake_speed += 1
+        
+        # Metodo para actualizar el objeto clock, recibe framerate para controlar velocidad
+        clock.tick(snake_speed)  
 
     pygame.quit() # Cerrar ventana juego
     quit() # Terminar programa
